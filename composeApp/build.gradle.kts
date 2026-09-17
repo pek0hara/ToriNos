930352c1ac33338f9474ca303eab2ebeb6f5308d
apple <bluelet.okudake@gmail.com>
2026-09-17 12:29:11 +0900

    chore: バージョンを1.0.8へ更新

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -137,8 +137,8 @@ android {
         applicationId = "com.nostr.torinos"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 13
-        versionName = "1.0.7"
+        versionCode = 14
+        versionName = "1.0.8"
     }
 
     buildFeatures {
